61b09ccbe4ea33cad245be1f483fb31011c121a4
CLARISSA CHEN <clarissachen5@gmail.com>
2024-10-22 11:44:55 +0100

    created expenses

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
+# Route for the index page
 @app.route('/')
-def home():
+def index():
     return render_template('index.html')
 
-if __name__ == '__main__':
+# Route for the expenses page
+@app.route('/expenses')
+def expenses():
+    return render_template('expense.html')
+
+if __name__ == "__main__":
     app.run(debug=True)
