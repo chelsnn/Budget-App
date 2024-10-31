15fca53d9aadd74673286d250802e87c8d98b1b1
apiando <ajohannp3@gmail.com>
2024-10-31 18:30:16 +0000

    Removed duplicate app variable definition from app.py
    Resolved merge conflict

--- app.py
+++ app.py
@@ -17,9 +17,6 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
     return conn
 
-
-#app = Flask(__name__)
-
 def create_users_table():
     conn = get_db_connection()
     conn.execute('''
